c0f7427daa42f6f8bea9f88587d5dfd851243e96
chaheekwon <96899408+nowkeehahc@users.noreply.github.com>
2023-04-20 22:41:04 +0900

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,52 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/models/weather.dart';
-import 'package:weather_app/services/weather_service.dart';
-import 'package:weather_app/widgets/weather_card.dart';
+import 'package:weather_app/screens/search_screen.dart';
+import 'package:weather_app/screens/weather_screen.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
+void main() {
+  runApp(MyApp());
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  final cityController = TextEditingController();
-  Weather? weather;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather App'),
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: cityController,
-                decoration: InputDecoration(
-                  hintText: 'Enter a city name',
-                  border: OutlineInputBorder(),
-                ),
-                onSubmitted: (value) async {
-                  final newWeather = await WeatherService.getWeather(value);
-                  setState(() {
-                    weather = newWeather;
-                  });
-                },
-              ),
-              SizedBox(height: 20),
-              if (weather != null)
-                WeatherCard(weather: weather!)
-              else
-                Text('Enter a city name to see the weather'),
-            ],
-          ),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WeatherScreen(),
+        '/search': (context) => SearchScreen(),
+      },
     );
   }
 }
